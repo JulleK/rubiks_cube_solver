2bcb3c61a47153b7e4b9594859cbb1ab4af6948b
Julian <97186369+JulleK@users.noreply.github.com>
2025-03-22 18:45:23 +0100

    diplay moves done by the algorithm in ascii

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,34 @@ import { log } from "console";
 import { CliCube } from "./cli.js";
 import { Solver } from "./solver.js";
 import { Cube } from "./cube.js";
+import { Move } from "./typings/cube_types.js";
+import { displayMoveAsAscii } from "./utils/move_ascii.js";
 
 const cube = new Cube();
 
 cube.applyMoves("F'", "U2", "F", "U2", "R", "U'", "R'");
+cube.scramble();
 
-const solver = new Solver(cube.getCubeState());
+const solver = new Solver(cube);
 
 solver.solveFirstLayer();
 
-// const moves = solver.getMoveHistory();
+const moves = solver.getMoveHistory();
 
-// for (const move of moves) {
-//     setTimeout(() => {
-//         // console.clear()
-//         log(move)
-//         cube.applyMoves(move)
-//         log(cube.visualizeCube())
-//     }, 1000)
-// }
+console.clear();
+log(cube.visualizeCube());
 
-// // const cli = new CliCube(solver.getCubeState());
-// // cli.promptMove();
+for (const move of moves) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.clear();
+  cube.applyMoves(move);
+  log(cube.visualizeCube());
+  //   log(move);
+  displayMoveAsAscii(move);
+}
+
+// log(solver.visualizeCube());
+// log(cube.visualizeCube());
+
+// const cli = new CliCube(solver.getCubeState());
+// cli.promptMove();
