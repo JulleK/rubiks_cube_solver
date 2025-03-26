aec8c0a3a38c9ee3123f8d7559c0517f3d44bd3a
Julian <97186369+JulleK@users.noreply.github.com>
2025-03-26 19:39:25 +0100

    add preset test scrambles, that previously the solver failed to solve

---
+++ src/utils/testScrambles.ts
@@ -0,0 +1,30 @@
+import { Cube2by2 } from "./typings/cube_types";
+
+export const scrambles: Array<Cube2by2> = [
+  [
+    "G",
+    "R",
+    "W",
+    "G",
+    "O",
+    "R",
+    "B",
+    "G",
+    "W",
+    "R",
+    "B",
+    "O",
+    "B",
+    "Y",
+    "G",
+    "R",
+    "Y",
+    "B",
+    "O",
+    "O",
+    "W",
+    "Y",
+    "Y",
+    "W",
+  ],
+];
